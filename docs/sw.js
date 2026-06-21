6c1da4d89db6eeee55295791ffdca99011ad8778
herbert <herbert@groot.jebbink.nl>
2026-06-21 14:14:21 +0200

    Add play card to home pages

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '2026-06-21-vram-auto-restart-1';
+const VERSION = '2026-06-21-home-play-card-1';
 const PRECACHE = `sneekie-precache-${VERSION}`;
 const RUNTIME = `sneekie-runtime-${VERSION}`;
 const KEEP_CACHES = new Set([PRECACHE, RUNTIME]);
